6c0d4f16286c0be5653b0df35f7772487cefe75d
Anshar23 <anshar.maulana@catatin.ai>
2025-10-08 12:59:28 +0700

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ["./**/*.{js,ts,tsx}", "./app/**/*.{js,ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
